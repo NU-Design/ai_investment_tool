099f985802f4992465eced76eafedd6a4fc69b1a
SitingT <tang.sit@northeastern.edu>
2024-02-03 22:28:34 -0800

    add popup

--- Desktop/financial model/financial_model/src/pages/CompanyPage.js
+++ Desktop/financial model/financial_model/src/pages/CompanyPage.js
@@ -3,6 +3,12 @@ import { useParams } from "react-router-dom";
 import CompanyChart from "../modules/CompanyChart";
 import ChatBot from "../modules/ChatBot";
 import { getOneCompanyQuotes } from "../services/CompanyDataApi";
+import Chatbot from "../modules/ChatBot";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 const CompanyPage = () => {
   const { symbol } = useParams();
@@ -10,6 +16,15 @@ const CompanyPage = () => {
     AAPL: "Apple",
     MSFT: "Microsoft",
     TSLA: "Tesla",
+  };
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   const [companyData, setCompanyData] = useState(null);
@@ -28,9 +43,18 @@ const CompanyPage = () => {
       <h1>{companyName}</h1>
 
       <CompanyChart companySymbol={symbol} />
-      <div>
-        <Button a href="/ChatBot">Ask Me</Button>
-      </div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open Dialog
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <ChatBot />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Finish</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
